4c1531e8cd0e74182aed8ab379057a2366fa08f8
mariamhammoud <hammoudmariam24@gmail.com>
2021-06-09 14:38:33 +0200

    issue: Neo4j Connection Function

---
+++ R/Connection Function.R
@@ -0,0 +1,35 @@
+#' Connect to Neo4j graph database
+#'
+#' @param url the Neo4j server url.
+#' @param user The username of the Neo4j server ("neo4j" is the default user).
+#' @param db The database name.
+#'
+#' @return A connection object to the Neo4j Server.
+#' @export
+#' @import neo4r
+#' @export
+#'
+#' @examples
+#' connection = connectToNeo4j()
+#' To check if the connection succeeded, use connection$ping()
+#' connection = connectToNeo4j()
+connectToNeo4j <- function(url, user, db) {
+  if(missing(url)){
+    stop("URL must be specified")
+  }
+  if(missing(user)){
+    stop("User must be specified")
+  }
+  if(missing(db)){
+    stop("Database Name must be specified")
+  }
+
+  connection <- neo4j_api$new(
+    url = url,
+    user = user,
+    password = rstudioapi::askForPassword(),
+    db = db
+    )
+
+  return(connection)
+}
