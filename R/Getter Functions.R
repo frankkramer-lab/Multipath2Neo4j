8d8d9e3374468d96a5b2fd42d4efce5d01e97c3f
mariamhammoud <hammoudmariam24@gmail.com>
2021-06-17 22:45:20 +0200

    Create getNodeAttributes Function

--- R/Getter Functions.R
+++ R/Getter Functions.R
@@ -81,3 +81,55 @@ isLabel <- function(connection, label){
 }
 
 
+#' Get the attributes of a node, or all nodes
+#'
+#' Get the attributes of a node, or all nodes
+#' @param connection The Neo4j connection object.
+#' @param property_key The node property key name to be identified by.
+#' @param property_key_value The started node property key value to be found within.
+#'
+#' @return A dataframe containing the attributes of the specified node. If the node property key and value are not specified, all nodes with their attributes are returned, in case of success, otherwise the result from the Neo4j call.
+#' @export
+#' @import neo4r
+#' @export
+#'
+#' @examples
+#' #To get all the nodes with their attributes
+#' result = getNodeAttributes(connection)
+#'
+#' #To get the nodes of name Complex70 with its attributes
+#' result = getNodeAttributes(connection,"name","Complex70")
+getNodeAttributes <- function(connection, property_key = NULL, property_key_value = NULL){
+  #If the function is called without passing a neo4j connection, raise an exception
+  if(missing(connection)){
+    stop("Neo4j Connection must be specified")
+  }
+  if(connection$ping() == FALSE){
+    stop("Check your connection")
+  }
+
+  #If the function is called by passing one of the arguments property key and value, raise an exception
+  if((missing(property_key) && !missing(property_key_value)) || (!missing(property_key) && missing(property_key_value))){
+    stop("Property key and value must be both NULL or specified")
+  }
+  if((!missing(property_key) && !missing(property_key_value)) && ((property_key == "" && property_key_value != "") || (property_key != "" && property_key_value == ""))){
+    stop("Property key and value must be both NULL or specified")
+  }
+
+  query = "MATCH (n"
+
+  if(!is.null(property_key) && property_key != ""){
+    query = paste0(query, " {", property_key, ": '", property_key_value ,"'}")
+  }
+  query = query = paste0(query, ") RETURN n")
+
+  result = call_neo4j(query, connection, type = c("row", "graph"), output = c("r","json"), include_stats = FALSE, include_meta = FALSE)
+
+  tmp = result
+  if(is.null(result$error_code))
+  {
+    tmp = as.data.frame(result,col.names = "")
+  }
+  return(tmp)
+}
+
