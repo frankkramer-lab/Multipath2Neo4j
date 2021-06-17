bafcbf05dd1f4c98822b45a6a09992f204c92bb7
mariamhammoud <hammoudmariam24@gmail.com>
2021-06-17 20:21:21 +0200

    Create isLabel Function

--- R/Getter Functions.R
+++ R/Getter Functions.R
@@ -33,4 +33,51 @@ getLabelsCount <- function(connection){
 }
 
 
+#' Verify if the label exists in a graph
+#'
+#' Verify if the label exists in a graph
+#' @param connection The Neo4j connection object.
+#' @param label The name of the label.
+#'
+#' @return A boolean value in case of success, otherwise the result from the Neo4j call.
+#'
+#' @export
+#' @import neo4r
+#' @export
+#'
+#' @examples
+#' #To get the labels count
+#' isLabel(connection, "DrUGs")
+isLabel <- function(connection, label){
+  #If the function is called without passing a neo4j connection, raise an exception
+  if(missing(connection)){
+    stop("Neo4j Connection must be specified")
+  }
+  if(connection$ping() == FALSE){
+    stop("Check your connection")
+  }
+
+  #If the function is called without passing a label, raise an exception
+  if(missing(label) || is.null(label) || label == ""){
+    stop("Node Label must be specified")
+  }
+
+  query = "CALL db.labels()"
+
+  result = call_neo4j(query, connection, type = c("row", "graph"), output = c("r","json"), include_stats = FALSE, include_meta = FALSE)
+
+  tmp = result
+  if(is.null(result$error_code))
+  {
+    labels_names = lapply(unlist(result), function(x){tolower(x)})
+    if (tolower(label) %in% labels_names) {
+      tmp = TRUE
+    }
+    else{
+      tmp = FALSE
+    }
+  }
+  return(tmp)
+}
+
 
